7059ecab8f4fb6710169bbbacbdd18b4f3d8a448
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2024-01-03 19:05:12 +0530

    added python code

--- aws-hc.py
+++ aws-hc.py
@@ -1,52 +1,96 @@
 import boto3
 import json
 import time
-import sys
 import os
+import subprocess
+from dotenv import load_dotenv
 
-input_image = sys.argv[1]
-file_name, file_extension = os.path.splitext(input_image)
+from picamera import PiCamera
+from time import sleep
+from gpiozero import Button
 
-s3 = boto3.client('s3')
+from urllib.request import urlopen
+from urllib.error import URLError
 
-# Create SQS client
-sqs = boto3.client('sqs')
-queue_url = 'https://sqs.us-east-1.amazonaws.com/556612833346/success-destination-queue'
+load_dotenv()
 
-BUCKET_NAME = 'haiku-cam-images'
-
-import uuid 
-upload_name = uuid.uuid4().hex[:10]
-
-s3.upload_file(input_image, BUCKET_NAME, '{}{}'.format(upload_name, file_extension))
-
-while (1):
-    print('Polling queue')
+# wait for internet connection
+while True:
     try:
-        # Receive message from SQS queue
-        response = sqs.receive_message(
-            QueueUrl=queue_url,
-            AttributeNames=[
-                'SentTimestamp'
-            ],
-            MaxNumberOfMessages=1,
-            MessageAttributeNames=[
-                'All'
-            ],
-            VisibilityTimeout=0,
-            WaitTimeSeconds=5
-        )
-        message = response['Messages'][0]
-        receipt_handle = message['ReceiptHandle']
-        print((json.loads(message["Body"])["responsePayload"])['body'])
-
-        # Delete received message from queue
-        sqs.delete_message(
-            QueueUrl=queue_url,
-            ReceiptHandle=receipt_handle
-        )
+        urlopen('https://www.google.com/',timeout=1)
         break
-    except:
-        print('Queue empty')
+    except URLError:
         pass
-    time.sleep(2)
+
+o = subprocess.check_output('hostname -I', shell=True)
+print(o)
+subprocess.run(["sudo /home/saarthak/ili9225spi_rpi/demo \"ip\" \"{}\" \"\"".format(o)], shell=True)
+
+camera = PiCamera()
+button = Button(17)
+
+while True:
+    print('wait for push...')
+    button.wait_for_press()
+    print('pushed')
+    sleep(2)
+    camera.capture('/home/saarthak/Desktop/test.jpg')
+    print('image saved')
+    subprocess.run(["sudo /home/saarthak/ili9225spi_rpi/demo \"load\" \"\" \"\""], shell=True)
+
+    input_image = '/home/saarthak/Desktop/test.jpg'
+    file_name, file_extension = os.path.splitext(input_image)
+
+    session = boto3.Session(
+        aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name = 'us-east-1'
+    )
+
+    s3 = session.client('s3')
+    sqs = session.client('sqs')
+    queue_url = os.getenv("QUEUE_URL")
+    BUCKET_NAME = os.getenv("BUCKET_NAME")
+
+    import uuid 
+    upload_name = uuid.uuid4().hex[:10]
+
+    s3.upload_file(input_image, BUCKET_NAME, '{}{}'.format(upload_name, file_extension))
+
+    while (1):
+        print('Polling queue')
+        try:
+            # Receive message from SQS queue
+            response = sqs.receive_message(
+                QueueUrl=queue_url,
+                AttributeNames=[
+                    'SentTimestamp'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ],
+                VisibilityTimeout=0,
+                WaitTimeSeconds=5
+            )
+            message = response['Messages'][0]
+            receipt_handle = message['ReceiptHandle']
+            response = json.loads((json.loads(message["Body"])["responsePayload"])['body'])
+            print((json.loads(message["Body"])["responsePayload"])['body'])
+            print(type(response))
+
+            # Delete received message from queue
+            sqs.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=receipt_handle
+            )
+            break
+        except:
+            print('Queue empty')
+            pass
+        time.sleep(2)
+
+    lines = response['haiku'].splitlines()
+    print(lines)
+
+    subprocess.run(["sudo /home/saarthak/ili9225spi_rpi/demo \"{}\" \"{}\" \"{}\"".format(lines[0], lines[1], lines[2])], shell=True)
---
+++ lambda_function.py
@@ -0,0 +1,55 @@
+import json
+import urllib.parse
+import boto3
+import requests
+import os
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+
+    # Get the object from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    img_url = f'https://{bucket}.s3.amazonaws.com/{key}'
+    print('THE URL IS: {}'.format(img_url))
+    
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {}".format(os.environ['OPENAI_SECRET_KEY']),
+    }
+    
+    data = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Write a Haiku poem about this image."},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": img_url,
+                        },
+                    },
+                ],
+            }
+        ],
+        "max_tokens": 300,
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    json_res = json.loads(response.text)
+    haiku = json_res['choices'][0]['message']['content']
+
+    output = {
+        "img_url": img_url,
+        "haiku": haiku
+    }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(output)
+    }
+
